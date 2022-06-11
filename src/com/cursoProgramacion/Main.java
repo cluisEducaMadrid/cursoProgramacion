5b7575f23068de878418fc49d878f04d219bf08d
Carlos Luis <cluis@educa.madrid.org>
2022-06-11 12:01:09 +0200

    Cambio nombre package
    (cherry picked from commit ab9fe90ba42106a66cdaf1a4ae13abc9acaf3153)

--- src/com/cursoProgramacion/Main.java
+++ src/com/cursoProgramacion/Main.java
@@ -1,8 +1,9 @@
-package com.company;
+package com.cursoProgramacion;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hola mundo");
     }
 }
