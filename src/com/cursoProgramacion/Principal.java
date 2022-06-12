8d84a64e1d679ef42f9eabad4de7912a6ae8ce0f
Carlos Luis <cluis@educa.madrid.org>
2022-06-12 12:06:21 +0200

    Primeras variables, primitivas

---
+++ src/com/cursoProgramacion/Principal.java
@@ -0,0 +1,27 @@
+/*
+*   Nombre de paquete donde agrupo los programas de mi aplicación
+*   Al cambiar el nombre del paquete donde está el método/programa main tengo
+*   que editar la configuración de la aplicación ("Application") e indicarle
+*   que el paquete para compilar y ejecutar ("Buid and Run) es com.cursoProgramación
+ */
+package com.cursoProgramacion;
+
+// Cambio el nombre de la clase "Main" por "Principal"
+// Tengo que cambiar también el nombre del fichero Main.java por Principal.java
+public class Principal {
+
+    //Este es el programa principal, que se llama main
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hola mundo");
+
+    // Variables primitivas.
+    short caracter = 51;
+    int entero = 51;
+    long largo = 51;
+    float flotante = 51.0F;
+    double doble =51.0D;
+    boolean verdadSiNo = false;
+    }
+    
+}
